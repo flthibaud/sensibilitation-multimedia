764e78cfef5aaa046317c630ff053de1a57f3ce6
Florian Thibaud <thibaud@wacan.com>
2026-09-10 10:30:43 +0200

    Fix latitude and altitude display in index.html and update function names in index.js

--- CartoTD1/Exo1/js/index.js
+++ CartoTD1/Exo1/js/index.js
@@ -1,9 +1,17 @@
-const getLongitude = (longitude) => {
+const setLongitude = (longitude) => {
   document.getElementById("longitude").textContent = longitude;
 }
 
-navigator.geolocation.getCurrentPosition((position) => {
-  getLongitude(position.coords.longitude);
-});
+const setLatitude = (latitude) => {
+  document.getElementById("latitude").textContent = latitude;
+}
 
-export { getLongitude };
+const setAltitude = (altitude) => {
+  document.getElementById("altitude").textContent = altitude;
+}
+
+navigator.geolocation.getCurrentPosition((position) => {
+  setLongitude(position.coords.longitude);
+  setLatitude(position.coords.latitude);
+  setAltitude(position.coords.altitude);
+});
